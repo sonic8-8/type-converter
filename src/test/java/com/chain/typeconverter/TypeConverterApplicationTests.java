b23287b59acb62e73501f69f4eefa611c4a5e823
이승환 <seaung13@gmail.com>
2025-01-09 15:25:21 +0900

    initial commit

---
+++ src/test/java/com/chain/typeconverter/TypeConverterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chain.typeconverter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TypeConverterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
