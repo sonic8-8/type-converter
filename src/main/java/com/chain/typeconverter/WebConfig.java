884a7d9a1626ea246d2c21ce4b5f1d349cda3dda
이승환 <seaung13@gmail.com>
2025-01-09 17:38:47 +0900

    feat: 스프링이 포맷터 적용하기 - MyNumberFormatter

--- src/main/java/com/chain/typeconverter/WebConfig.java
+++ src/main/java/com/chain/typeconverter/WebConfig.java
@@ -4,6 +4,7 @@ import com.chain.typeconverter.converter.IntegerToStringConverter;
 import com.chain.typeconverter.converter.IpPortToStringConverter;
 import com.chain.typeconverter.converter.StringToIntegerConverter;
 import com.chain.typeconverter.converter.StringToIpPortConverter;
+import com.chain.typeconverter.formatter.MyNumberFormatter;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.format.FormatterRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -13,10 +14,11 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addFormatters(FormatterRegistry registry) {
-        registry.addConverter(new StringToIntegerConverter());
-        registry.addConverter(new IntegerToStringConverter());
+//        registry.addConverter(new StringToIntegerConverter());
+//        registry.addConverter(new IntegerToStringConverter());
         registry.addConverter(new StringToIpPortConverter());
-        registry.addConverter(new IpPortToStringConverter())
-        ;
+        registry.addConverter(new IpPortToStringConverter());
+
+        registry.addFormatter(new MyNumberFormatter());
     }
 }
